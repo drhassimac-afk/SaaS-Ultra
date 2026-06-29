b6b4f7964ac7302ebc3e623e63dbd7830ff13828
RabahDj <drhassimac@gmail.com>
2026-06-29 15:57:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from functools import wraps
 from werkzeug.utils import secure_filename
 from datetime import datetime
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='.')
 app.secret_key = "SUPER_SECRET_SAAS_KEY_2026"  # مفتاح تشفير الجلسات
 
 # ================= DATABASE UTILITIES =================
